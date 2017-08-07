084f1e217703ef6b8e90231aafb4d8baf43d6813
KirillMysnik <kirill@mysnik.com>
2017-08-07 07:23:13 +0300

    PyPi ready

---
+++ README.rst
@@ -0,0 +1,36 @@
+OBS WebSocket RemoteControl
+===========================
+
+What's this?
+------------
+**obs-ws-rc** is a Python 3.5+ library that allows you to establish client
+connections to the `obs-websocket <https://github.com/Palakis/obs-websocket/>`_
+plugin for OBS Studio.
+
+It uses an **asyncio**-based approach which it inherited from the underlying
+WebSocket library - `websocket <https://github.com/aaugustin/websockets/>`_
+
+Installation
+------------
+The library is `available via PyPi <https://pypi.python.org/pypi/obs-ws-rc>`_:
+
+``pip install obs-ws-rc``
+
+Source Code
+-----------
+GitHub repository: `github.com/KirillMysnik/obs-ws-rc/
+<https://github.com/KirillMysnik/obs-ws-rc/>`_
+
+Documentation
+-------------
+The docs are `hosted at Read the Docs <http://obs-ws-rc.readthedocs.io>`_
+
+Examples
+--------
+Sample code is located in the `example folder
+<https://github.com/KirillMysnik/obs-ws-rc/tree/master/examples>`_
+
+License
+-------
+Available via `MIT License
+<https://github.com/KirillMysnik/obs-ws-rc/blob/master/LICENSE>`_
---
+++ setup.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+from setuptools import setup
+
+
+root = Path(__file__).parent.absolute()
+
+
+with open(root / 'README.rst') as f:
+    long_description = f.read()
+
+
+setup(
+    name='obs-ws-rc',
+
+    version='1.0',
+
+    description=("asyncio-based Python 3.5+ client to obs-websocket "
+                 "plugin for OBS Studio"),
+
+    long_description=long_description,
+
+    url="https://github.com/KirillMysnik/obs-ws-rc",
+
+    author="Kirill Mysnik",
+
+    author_email = "kirill@mysnik.com",
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Multimedia :: Video',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Operating System :: OS Independent',
+
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='obs obs-websocket',
+
+    packages=['obswsrc', ],
+
+    install_requires=['websockets', ],
+
+    python_requires='>=3.5',
+
+    package_data={
+        'obswsrc': ["protocol.json", ],
+    }
+)
